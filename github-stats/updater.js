de27c4ea69d8cd663d88b79b9b182e94e78647eb
William Harrison <william@williamdavidharrison.com.au>
2023-03-19 12:51:04 +0800

    fix undefined class

--- github-stats/updater.js
+++ github-stats/updater.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 const nunjucks = require("nunjucks");
-const { GithubUser } = require("./src/github");
+const { GitHubUser } = require("./src/github");
 const themes = require("./src/theme");
 
 nunjucks.configure({ autoescape: true });
@@ -11,7 +11,7 @@ let templateString = fs.readFileSync("./template.svg", "utf-8");
 let username = process.argv[2];
 let req_theme = process.argv[3] || "random";
 
-let user = new GithubUser(username);
+let user = new GitHubUser(username);
 user.fetchContent().then(() => {
     let outString = nunjucks.renderString(templateString, {
         data: user,
